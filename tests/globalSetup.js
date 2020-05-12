ca5781f336c529fe6ef7a06d354008514f94aff2
TanguyH <tanguy.herbron@outlook.com>
2020-05-12 14:13:11 +0200

    Update global setup and teardown to have more control over the server

--- tests/globalSetup.js
+++ tests/globalSetup.js
@@ -5,4 +5,7 @@ const server = require("../main");
 module.exports = async () => {
     global.httpServer = server;
     await global.httpServer.listen();
+    
+    // Waiting for the first update of the subdomain_retriever
+    await new Promise(resolve => setTimeout(resolve, 500));
 };
--- tests/globalTeardown.js
+++ tests/globalTeardown.js
@@ -1,3 +1,3 @@
 module.exports = async () => {
-    await global.httpServer.stop();
+    process.exit(0);
 };
